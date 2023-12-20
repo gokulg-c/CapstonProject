369be4a9ac17c67e4ee9a61a4c07ad1e0951504a
160amit <160amit>
2023-12-20 11:36:52 +0000

    update

--- module3/streaming data.py
+++ module3/streaming data.py
@@ -15,22 +15,6 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
-display(dbutils.fs.ls("/mnt/wetelcostreams"))
-
-# COMMAND ----------
-
-df=spark.read.parquet("/mnt/wetelcostreams")
-
-# COMMAND ----------
-
-df.printSchema()
-
-# COMMAND ----------
-
-display(df)
-
-# COMMAND ----------
-
 container_name = "mytelco"
 account_name = "capstoneproject045"
 storage_account_key = "YomsEYQH5ict2ov6Ld2NoBbxT3eFhjIQDAWGgmSxW2eHiD4mSu48DStIIKCqqvylq5fiRupFU1xv+ASt3UQakA=="
@@ -42,51 +26,41 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
-display(dbutils.fs.mounts())
+@dlt.create_table(
+  comment="The raw customers stream data.",
+  table_properties={
+    "WeEnsure_delta.quality": "bronze",
+    "pipelines.autoOptimize.managed": "true"
+  }
+)
 
-# COMMAND ----------
+def customer_stream_raw():
+    """
+    Load and create a Delta Lake table for raw customer streaming data.
 
-# reading the streaming data
-stream_df = spark.readStream \
-    .format("cloudFiles") \
-    .option("cloudFiles.format", "parquet") \
-    .option("cloudFiles.schemaLocation", "/mnt/wetelcoschema/outputschema") \
-    .option("cloudFiles.schemaEvolutionMode","rescue") \
-    .load("/mnt/wetelcostreams/*.parquet")
-column_names = stream_df.columns
-for column_name in column_names:
-  stream_df = stream_df.withColumnRenamed(column_name, column_name.replace(" ", "_"))
+    This function reads streaming data in Parquet format from a specified location
+    and creates a Delta Lake table for the raw customer streaming data.
+
+    Returns:
+        pyspark.sql.DataFrame: A Spark SQL DataFrame containing the raw customer streaming data.
+    """
+    # reading the streaming data
+    stream_df = spark.readStream \
+        .format("cloudFiles") \
+        .option("cloudFiles.format", "parquet") \
+        .option("cloudFiles.schemaLocation", "/mnt/wetelcoschema/outputschema") \
+        .option("cloudFiles.schemaEvolutionMode","rescue") \
+        .load("/mnt/wetelcostreams/*.parquet")
+    column_names = stream_df.columns
+    for column_name in column_names:
+    stream_df = stream_df.withColumnRenamed(column_name, column_name.replace(" ", "_"))
 # Assuming streaming_df is your Spark DataFrame
-stream_df = stream_df.toDF(*[col.lower() for col in stream_df.columns])
-
-
-# COMMAND ----------
-
-stream_df.display()
+    stream_df = stream_df.toDF(*[col.lower() for col in stream_df.columns])
+    return stream_df
 
 # COMMAND ----------
 
-paths = {
-    "plan_df": "/mnt/basedata/SilverLayerData/Plans/",
-    "customer_rating_df": "/mnt/basedata/SilverLayerData/Customer_Rating/",
-    "customer_information_df": "/mnt/basedata/SilverLayerData/Customer_Information/",
-    "device_information_df": "/mnt/basedata/SilverLayerData/Device_Information/",
-    "billing_partition_df": "/mnt/basedata/SilverLayerData/Billing_Information/"
-}
-# Assuming you have loaded DataFrames for each path, for example:
-plan_df = spark.read.parquet(paths["plan_df"])
-customer_rating_df = spark.read.parquet(paths["customer_rating_df"])
-customer_information_df = spark.read.parquet(paths["customer_information_df"])
-device_information_df = spark.read.parquet(paths["device_information_df"])
-billing_partition_df = spark.read.parquet(paths["billing_partition_df"])
-
-# COMMAND ----------
-
-display(customer_information_df)
-
-# COMMAND ----------
-
-stream_df.columns
+# customer_information_df = spark.read.parquet("/mnt/basedata/SilverLayerData/")
 
 # COMMAND ----------
 
@@ -95,18 +69,22 @@ stream_df.columns
 
 # COMMAND ----------
 
-joined_stream_df = stream_df.join(
-    customer_information_df,
-    (stream_df.caller_number == customer_information_df.customer_phone) |
-    (stream_df.receiver_number == customer_information_df.customer_phone),
-    "inner"
+@dlt.create_table(
+  comment="The raw customers stream data.",
+  table_properties={
+    "WeEnsure_delta.quality": "gold",
+    "pipelines.autoOptimize.managed": "true"
+  }
 )
-display(joined_stream_df)
-
-# COMMAND ----------
-
-query = joined_stream_df.writeStream.outputMode("append").format("console").start()
-
+def fraudIdentification():
+    stream_df = dlt.read("customer_stream_raw")
+    joined_stream_df = stream_df.join(
+        customer_information_df,
+        (stream_df.caller_number == customer_information_df.customer_phone) |
+        (stream_df.receiver_number == customer_information_df.customer_phone),
+        "inner"
+    )
+    return joined_stream_df
 
 # COMMAND ----------
 
@@ -115,26 +93,26 @@ query = joined_stream_df.writeStream.outputMode("append").format("console").star
 
 # COMMAND ----------
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, unix_timestamp, from_utc_timestamp
+from pyspark.sql.functions import *
+@dlt.create_table(
+  comment="duration of call by suspected region",
+  table_properties={
+    "WeEnsure_delta.quality": "gold",
+    "pipelines.autoOptimize.managed": "true"
+  }
+)
+def average_duration_by_reason():
+    stream_df = dlt.read("customer_stream_raw")
+    # Convert start_time and end_time to timestamp
+    stream_df = stream_df.withColumn("start_time", from_utc_timestamp(unix_timestamp("start_time", "yyyy-MM-dd HH:mm:ss").cast("timestamp"), "UTC"))
+    stream_df = stream_df.withColumn("end_time", from_utc_timestamp(unix_timestamp("end_time", "yyyy-MM-dd HH:mm:ss").cast("timestamp"), "UTC"))
 
+    # Calculate duration in seconds
+    stream_df = stream_df.withColumn("call_duration", (unix_timestamp("end_time") - unix_timestamp("start_time")))
 
-# Convert start_time and end_time to timestamp
-stream_df = stream_df.withColumn("start_time", from_utc_timestamp(unix_timestamp("start_time", "yyyy-MM-dd HH:mm:ss").cast("timestamp"), "UTC"))
-stream_df = stream_df.withColumn("end_time", from_utc_timestamp(unix_timestamp("end_time", "yyyy-MM-dd HH:mm:ss").cast("timestamp"), "UTC"))
-
-# Calculate duration in seconds
-stream_df = stream_df.withColumn("call_duration", (unix_timestamp("end_time") - unix_timestamp("start_time")))
-
-# Group by suspected_reason and calculate the average duration
-average_duration_by_reason = stream_df.groupBy("suspected_reason").agg({"call_duration": "avg"})
-
-
-
-
-# COMMAND ----------
-
-display(average_duration_by_reason)
+    # Group by suspected_reason and calculate the average duration
+    average_duration_by_reason = stream_df.groupBy("suspected_reason").agg({"call_duration": "avg"})    
+    return average_duration_by_reason
 
 # COMMAND ----------
 
@@ -143,11 +121,17 @@ display(average_duration_by_reason)
 
 # COMMAND ----------
 
-fraud_calls_by_location = stream_df.groupBy("location") \
-    .agg({"location": "count"}) \
-    .withColumnRenamed("count(location)", "no_of_fraud_calls")
-display(fraud_calls_by_location)
-
-# COMMAND ----------
-
-fraud_calls_by_location.columns
+from pyspark.sql.functions import *
+@dlt.create_table(
+  comment="fraud_calls_by_location",
+  table_properties={
+    "WeEnsure_delta.quality": "gold",
+    "pipelines.autoOptimize.managed": "true"
+  }
+)
+def fraud_calls_by_location():
+    stream_df = dlt.read("customer_stream_raw")
+    fraud_calls_by_location = stream_df.groupBy("location") \
+        .agg({"location": "count"}) \
+        .withColumnRenamed("count(location)", "no_of_fraud_calls")
+    return fraud_calls_by_location
